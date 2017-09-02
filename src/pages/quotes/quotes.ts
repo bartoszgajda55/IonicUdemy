7e0c3c935fcd9936f9a8801ca949e550c70843cc
Bartosz Gajda <bg@bartoszgajda.com>
2017-09-02 17:43:02 +0100

    Adding alerts to app.

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {IonicPage, NavParams} from 'ionic-angular';
+import {AlertController, IonicPage, NavParams} from 'ionic-angular';
 import {Quote} from "../../data/quote.interface";
 
 @IonicPage()
@@ -10,17 +10,37 @@ import {Quote} from "../../data/quote.interface";
 export class QuotesPage implements OnInit{
   quoteGroup: { category: string, quotes: Quote[], icon: string };
 
-  constructor(private navParams: NavParams) {}
-
-  // ionViewDidLoad() {
-  //   this.quoteGroup = this.navParams.data;
-  // Add elvis operator (?) in template
-  // }
-  // ionViewDidLoad is executed after the template is rendered
-  // ngOnInit is executed before it.
+  constructor(
+    private navParams: NavParams,
+    private alertCtrl: AlertController) {}
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
+  }
+
+  onAddToFavourite(slectedQuote: Quote) {
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure ?',
+      message: 'Are you sure you want to add the quote ?',
+      buttons: [
+        {
+          text: 'Yes',
+          handler: () => {
+            console.log('Ok');
+          }
+        },
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => {
+            console.log('Nope');
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 
 }
