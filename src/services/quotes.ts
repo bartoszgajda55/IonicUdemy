fdc78bfb3771ab7f04982fadf47b43c337f69a9f
Bartosz Gajda <bg@bartoszgajda.com>
2017-09-07 19:58:47 +0100

    Adding Angular style Service.

---
+++ src/services/quotes.ts
@@ -0,0 +1,20 @@
+import {Quote} from "../data/quote.interface";
+
+export class QuotesService {
+  private favouriteQuotes: Quote[] = [];
+
+  addQuoteToFavourites(quote: Quote) {
+    this.favouriteQuotes.push(quote);
+  }
+
+  removeQuoteFromFavourite(quote: Quote) {
+    const position = this.favouriteQuotes.findIndex((quoteElement: Quote) => {
+      return quoteElement.id == quote.id;
+    })
+    this.favouriteQuotes.splice(position, 1);
+  }
+
+  getFavouriteQuotes(): Quote[] {
+    return this.favouriteQuotes.slice();
+  }
+}
