705b2e958853ce0c677bde63cdfb7b24ae388a6e
Bartosz Gajda <bg@bartoszgajda.com>
2017-10-14 17:47:41 +0100

    Displaying items from Shopping List.

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 import {NgForm} from "@angular/forms";
 import {ShoppingListService} from "../../services/shopping-list";
+import {Ingredient} from "../../models/ingredient";
 
 @IonicPage()
 @Component({
@@ -9,10 +10,21 @@ import {ShoppingListService} from "../../services/shopping-list";
   templateUrl: 'shopping-list.html',
 })
 export class ShoppingListPage {
+  listItems: Ingredient[];
+
   constructor(private shoppingListService: ShoppingListService) {}
+
+  ionViewWillEnter() {
+    this.loadItems();
+  }
 
   onAddItem(form: NgForm) {
     this.shoppingListService.addItem(form.value.ingredientName, form.value.amount);
     form.reset();
+    this.loadItems();
+  }
+
+  private loadItems() {
+    this.listItems = this.shoppingListService.getItems();
   }
 }
