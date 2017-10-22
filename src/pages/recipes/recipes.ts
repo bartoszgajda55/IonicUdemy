295ef7ece75213ae6ff8d295e55619fd4bd6de26
Bartosz Gajda <bg@bartoszgajda.com>
2017-10-22 17:09:11 +0100

    Outputting recipes.

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {IonicPage, NavController} from 'ionic-angular';
 import {EditRecipePage} from "../edit-recipe/edit-recipe";
+import {Recipe} from "../../models/recipe";
+import {RecipesService} from "../../services/recipes";
 
 @IonicPage()
 @Component({
@@ -8,10 +10,22 @@ import {EditRecipePage} from "../edit-recipe/edit-recipe";
   templateUrl: 'recipes.html',
 })
 export class RecipesPage {
+  private recipes: Recipe[];
 
-  constructor(private navCtrl: NavController) {}
+  constructor(
+    private navCtrl: NavController,
+    private recipesService: RecipesService
+    ) {}
+
+  ionViewWillEnter() {
+    this.recipes = this.recipesService.getRecipes();
+  }
 
   onNewRecipe() {
     this.navCtrl.push(EditRecipePage, {mode: 'New'});
+  }
+
+  public onLoadRecipe(): void {
+
   }
 }
