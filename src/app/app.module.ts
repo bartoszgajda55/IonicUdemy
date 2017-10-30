e362b31ff0b4cd6e54f6abc161c4ed6cf15f0ece
Bartosz Gajda <bgajda@red-paint.com>
2017-10-30 14:41:12 +0000

    Generating required pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,26 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import {HomePage} from "../pages/home/home";
+import {AddPlacePage} from "../pages/add-place/add-place";
+import {PlacePage} from "../pages/place/place";
+import {SetLocationPage} from "../pages/set-location/set-location";
+
+const PAGES = [
+  MyApp,
+  HomePage,
+  AddPlacePage,
+  PlacePage,
+  SetLocationPage
+];
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: PAGES,
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: PAGES,
   providers: [
     StatusBar,
     SplashScreen,
