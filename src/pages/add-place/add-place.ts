7d78f89d0ebb10acb40add31661e164817d272fb
Bartosz Gajda <bgajda@red-paint.com>
2017-10-30 15:49:00 +0000

    New place page form.

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {NgForm} from "@angular/forms";
 
-/**
- * Generated class for the AddPlacePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-add-place',
@@ -14,11 +9,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddPlacePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams
+  ) { }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddPlacePage');
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
-
 }
