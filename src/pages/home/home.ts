55fde16e6808da807dbcdeb422bf2bb619892654
Bartosz Gajda <bgajda@red-paint.com>
2017-10-30 14:50:11 +0000

    Implementing navigation.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,24 +1,18 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AddPlacePage} from "../add-place/add-place";
 
-/**
- * Generated class for the HomePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
 })
 export class HomePage {
+  addPlacePage = AddPlacePage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams
+  ) { }
 
 }
