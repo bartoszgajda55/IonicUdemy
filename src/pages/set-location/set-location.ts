3b836f8e4c000cbbe7fbb5f9bad90410239397fb
Bartosz Gajda <bg@bartoszgajda.com>
2017-11-07 18:01:43 +0000

    Passing location to page.

--- src/pages/set-location/set-location.ts
+++ src/pages/set-location/set-location.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
 import {Location} from "../../models/location";
 
 @IonicPage()
@@ -12,11 +12,22 @@ export class SetLocationPage {
   marker: Location;
 
   constructor(public navCtrl: NavController,
-              public navParams: NavParams) {
+              public navParams: NavParams,
+              private viewCtrl: ViewController) {
     this.location = this.navParams.get('data');
   }
 
   onSetMarker(event: any): void {
     this.marker = new Location(event.coords.lat, event.coords.lng);
+  }
+
+  onConfirm(): void {
+    this.viewCtrl.dismiss({
+      location: this.marker
+    });
+  }
+
+  onAbort(): void {
+    this.viewCtrl.dismiss();
   }
 }
