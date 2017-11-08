4a4c048d0c2a5e5e92b313c7655838639519f9fa
Bartosz Gajda <bg@bartoszgajda.com>
2017-11-08 19:53:54 +0000

    Configuring single place page.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, ModalController, NavController, NavParams} from 'ionic-angular';
 import {AddPlacePage} from "../add-place/add-place";
 import {Place} from "../../models/place";
 import {PlacesService} from "../../services/places.service";
+import {PlacePage} from "../place/place";
 
 @IonicPage()
 @Component({
@@ -13,14 +14,17 @@ export class HomePage {
   addPlacePage = AddPlacePage;
   places: Place[] = [];
 
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    private placesService: PlacesService
-  ) { }
+  constructor(private placesService: PlacesService,
+              private modalCtrl: ModalController) {
+  }
 
   ionViewWillEnter() {
     this.places = this.placesService.getPlaces();
+  }
+
+  onOpenPlace(place: Place): void {
+    const modal = this.modalCtrl.create(PlacePage, {place: place});
+    modal.present();
   }
 
 }
