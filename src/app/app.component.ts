d2b8823de778e167861b14e1e438f3a8c197d522
Bartosz Gajda <bg@bartoszgajda.com>
2017-11-12 19:37:35 +0000

    Cleaning...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,67 +1,25 @@
 import {Component, ViewChild} from '@angular/core';
-import {MenuController, NavController, Platform} from 'ionic-angular';
+import {Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import {TabsPage} from "../pages/tabs/tabs";
-import {SigninPage} from "../pages/signin/signin";
-import {SignupPage} from "../pages/signup/signup";
-import firebase from 'firebase';
-import {AuthService} from "../services/auth";
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-
-  tabsPage:any = TabsPage;
-  signinPage = SigninPage;
-  signupPage = SignupPage;
-  isAuthenticated = false;
-  @ViewChild('nav') nav: NavController;
+  tabsPage:any = null;
 
   constructor(
     platform: Platform,
     statusBar: StatusBar,
     splashScreen: SplashScreen,
-    private menuCtrl: MenuController,
-    private authService: AuthService
   ) {
-     firebase.initializeApp({
-       apiKey: "AIzaSyBfcfrtBVQz22Qp7lxHZEF8YzxqT6h9eYU",
-       authDomain: "ionic-recipebook-7369b.firebaseapp.com",
-       databaseURL: "https://ionic-recipebook-7369b.firebaseio.com",
-       projectId: "ionic-recipebook-7369b",
-       storageBucket: "ionic-recipebook-7369b.appspot.com",
-       messagingSenderId: "291799003450"
-     });
-
-     firebase.auth().onAuthStateChanged(user => {
-       if (user) {
-        this.isAuthenticated = true;
-        this.nav.setRoot(this.tabsPage);
-       } else {
-         this.isAuthenticated = false;
-         this.nav.setRoot(this.signinPage);
-       }
-     });
-
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
-  }
-
-  onLoad(page: any): void {
-    this.nav.setRoot(page);
-    this.menuCtrl.close();
-  }
-
-  onLogout(): void {
-    this.authService.logout();
-    this.menuCtrl.close();
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,55 +7,22 @@ import {ErrorHandler, NgModule} from "@angular/core";
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import {EditRecipePage} from "../pages/edit-recipe/edit-recipe";
-import {RecipePage} from "../pages/recipe/recipe";
-import {RecipesPage} from "../pages/recipes/recipes";
-import {ShoppingListPage} from "../pages/shopping-list/shopping-list";
-import {TabsPage} from "../pages/tabs/tabs";
-import {ShoppingListService} from "../services/shopping-list";
-import {RecipesService} from "../services/recipes";
-import {SigninPage} from "../pages/signin/signin";
-import {SignupPage} from "../pages/signup/signup";
-import {AuthService} from "../services/auth";
-import {DatabaseOptionsPage} from "../pages/database-options/database-options";
-import {HttpModule} from "@angular/http";
-
 @NgModule({
   declarations: [
-    MyApp,
-    EditRecipePage,
-    RecipePage,
-    RecipesPage,
-    ShoppingListPage,
-    TabsPage,
-    SigninPage,
-    SignupPage,
-    DatabaseOptionsPage
+    MyApp
   ],
   imports: [
     BrowserModule,
-    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    EditRecipePage,
-    RecipePage,
-    RecipesPage,
-    ShoppingListPage,
-    TabsPage,
-    SigninPage,
-    SignupPage,
-    DatabaseOptionsPage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    ShoppingListService,
-    RecipesService,
-    AuthService
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
